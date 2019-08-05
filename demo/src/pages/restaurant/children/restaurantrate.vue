7e921d040ea3f6a82c5e8e8ef45eefbc54f37f36
M3VMKNUWQBY8Z9M\Administrator <978105623@qq.com>
2019-08-05 20:12:19 +0800

    no message

---
+++ demo/src/pages/restaurant/children/restaurantrate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="s-rate">评分页面</div>
+</template>
+
+<script>
+    export default {
+        name: "restaurantrate"
+    }
+</script>
+
+<style scoped lang="less">
+.s-rate{
+  margin-top: 5.3rem;
+}
+</style>
