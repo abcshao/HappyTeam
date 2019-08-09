e3809aef6ad97ceab61041ab9e59d2670e5cf3ac
lanou\lanou <554845282@qq.com>
2019-08-09 21:06:20 +0800

    no message

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
 
     <div class="s-body">
+
+      <transition enter-active-class="animated fadeIn" leave-active-class="opacity 0">
       <router-view/>
+      </transition>
+      
     </div>
   </div>
 </template>
